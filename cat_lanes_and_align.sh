3bc8c4d7476e61c190f5257605f124072d62429a
Corbin <31924492+C-Corbin@users.noreply.github.com>
2017-12-14 11:15:12 +0000

    Flagstat metrics
    Generated flagstat metrics with samtools

--- cat_lanes_and_align.sh
+++ cat_lanes_and_align.sh
@@ -1,5 +1,5 @@
-#run from example_fastqs directory
-#join lanes 1 and 2, writes to the joined_lanes folder 
+#Run the following from example_fastqs directory
+#Join lanes 1 and 2, writes to the joined_lanes folder 
 #cat 1504850-S1509352-02_GCTCGGTA_L001_R1_001.fastq.gz 1504850-S1509352-02_GCTCGGTA_L002_R1_001.fastq.gz > ../home/stpuser/joined_lanes/1504850-S1509352-02_GCTCGGTA_R1_both.fastq.gz
 #cat 1504850-S1509352-02_GCTCGGTA_L001_R2_001.fastq.gz 1504850-S1509352-02_GCTCGGTA_L002_R2_001.fastq.gz > ../home/stpuser/joined_lanes/1504850-S1509352-02_GCTCGGTA_R2_both.fastq.gz
 #cat 1606034-S1612259-02_CGAACTTA_L001_R1_001.fastq.gz 1606034-S1612259-02_CGAACTTA_L002_R1_001.fastq.gz > ../home/stpuser/joined_lanes/1606034-S1612259-02_CGAACTTA_R1_both.fastq.gz
@@ -11,8 +11,8 @@
 #cat 1703057-S1705957-02_AAGACGGA_L001_R1_001.fastq.gz 1703057-S1705957-02_AAGACGGA_L002_R1_001.fastq.gz > ../home/stpuser/joined_lanes/1703057-S1705957-02_AAGACGGA_R1_both.fastq.gz
 #cat 1703057-S1705957-02_AAGACGGA_L001_R2_001.fastq.gz 1703057-S1705957-02_AAGACGGA_L002_R2_001.fastq.gz > ../home/stpuser/joined_lanes/1703057-S1705957-02_AAGACGGA_R2_both.fastq.gz
 
-#run bwa mem on R1 and R2 for every sample, writes to ../../home/stpuser/aligned_seqs
-#the genome reference is in ../../reference_files/ucsc.hg19.nohap.masked.fasta
+#Run bwa mem on R1 and R2 for every sample, writes to ../../home/stpuser/aligned_seqs
+#The genome reference is in ../../reference_files/ucsc.hg19.nohap.masked.fasta
 
 #cd ../software/bwa-0.7.15
 #bwa mem ../../reference_files/ucsc.hg19.nohap.masked.fasta ../../home/stpuser/joined_lanes/1504850-S1509352-02_GCTCGGTA_R1_both.fastq.gz ../../home/stpuser/joined_lanes/1504850-S1509352-02_GCTCGGTA_R2_both.fastq.gz | samtools view -b -o ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA.bam
@@ -21,7 +21,7 @@
 #bwa mem ../../reference_files/ucsc.hg19.nohap.masked.fasta ../../home/stpuser/joined_lanes/1609778-S1620040-02_CGCTGATC_R1_both.fastq.gz ../../home/stpuser/joined_lanes/1609778-S1620040-02_CGCTGATC_R2_both.fastq.gz | samtools view -b -o ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC.bam
 #bwa mem ../../reference_files/ucsc.hg19.nohap.masked.fasta ../../home/stpuser/joined_lanes/1703057-S1705957-02_AAGACGGA_R1_both.fastq.gz ../../home/stpuser/joined_lanes/1703057-S1705957-02_AAGACGGA_R1_both.fastq.gz | samtools view -b -o ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA.bam
 
-#sort the files
+#Sort the files
 #cd ../samtools-1.3.1
 #samtools sort -o ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted.bam ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA.bam
 #samtools sort -o ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted.bam ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA.bam
@@ -29,23 +29,26 @@
 #samtools sort -o ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted.bam ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC.bam
 #samtools sort -o ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted.bam ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA.bam
 
-#index the files for IGV viewing
+#Index the files for IGV viewing
 #samtools index ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted.bam
 #samtools index ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted.bam
 #samtools index ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted.bam
 #samtools index ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted.bam
 #samtools index ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted.bam
 
-#need to mark duplicates for each file
-#but first I need a metrics file
-cd ../picard-tools-2.5.0 
+#I need to mark duplicates for each file
+#This also writes a metrics file for each flagged file
+#cd ../picard-tools-2.5.0 
 #java -jar picard.jar MarkDuplicates I=../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted.bam O=../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag.bam M=../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_metrics.txt
 #java -jar picard.jar MarkDuplicates I=../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted.bam O=../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted_dupflag.bam M=../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted_dupflag_metrics.txt
-java -jar picard.jar MarkDuplicates I=../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted.bam O=../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag.bam M=../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag_metrics.txt
-java -jar picard.jar MarkDuplicates I=../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted.bam O=../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag.bam M=../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag_metrics.txt
-java -jar picard.jar MarkDuplicates I=../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted.bam O=../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag.bam M=../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag_metrics.txt
+#java -jar picard.jar MarkDuplicates I=../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted.bam O=../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag.bam M=../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag_metrics.txt
+#java -jar picard.jar MarkDuplicates I=../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted.bam O=../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag.bam M=../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag_metrics.txt
+#java -jar picard.jar MarkDuplicates I=../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted.bam O=../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag.bam M=../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag_metrics.txt
 
-
-
-
-
+#samtools flagstat for quality metrics - to see how they match up with the picard ones!
+#cd samtools-1.3.1
+#samtools flagstat ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag.bam > ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_flagstat.txt
+#samtools flagstat ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted_dupflag.bam > ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted_dupflag_flagstat.txt
+#samtools flagstat ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag.bam > ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag_flagstat.txt
+#samtools flagstat ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag.bam > ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag_flagstat.txt
+#samtools flagstat ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag.bam > ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag_flagstat.txt
