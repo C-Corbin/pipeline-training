454cf1c5c4bb3e7f3e7886a9d55ab77b276d8144
Corbin <31924492+C-Corbin@users.noreply.github.com>
2017-12-21 14:31:53 +0000

    Call variants and start annotation
    Called variants with HaplotypeCaller, some SnpEff annotation to GRCh37, need to add decomposition

--- cat_lanes_and_align.sh
+++ cat_lanes_and_align.sh
@@ -229,53 +229,53 @@
 #1504850-S1509352-02_GCTCGGTA - NGD Complete
 #cd ../GenomeAnalysisTK-3.6
 #java -jar GenomeAnalysisTK.jar \
-	-T RealignerTargetCreator \
-	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
-	-I ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG.bam \
-	-o ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG.intervals
+#	-T RealignerTargetCreator \
+#	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
+#	-I ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG.bam \
+#	-o ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG.intervals
 #
 #1606034-S1612259-02_CGAACTTA - CTD
 #java -jar GenomeAnalysisTK.jar \
-	-T RealignerTargetCreator \
-	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
-	-L ../../example_fastqs/beds/CTDFinaldesignwith25bp_v3.bed \
-	-I ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted_dupflag_RG.bam \
-	-o ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted_dupflag_RG.intervals
+#	-T RealignerTargetCreator \
+#	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
+#	-L ../../example_fastqs/beds/CTDFinaldesignwith25bp_v3.bed \
+#	-I ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted_dupflag_RG.bam \
+#	-o ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted_dupflag_RG.intervals
 #
 #1607686-S1615531-02_TTCACGCA - Motor Complete
 #java -jar GenomeAnalysisTK.jar \
-	-T RealignerTargetCreator \
-	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
-	-L ../../example_fastqs/beds/Motor_CompletePanel_v1.bed \
-	-I ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag_RG.bam \
-	-o ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag_RG.intervals
+#	-T RealignerTargetCreator \
+#	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
+#	-L ../../example_fastqs/beds/Motor_CompletePanel_v1.bed \
+#	-I ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag_RG.bam \
+#	-o ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag_RG.intervals
 #
 #1609778-S1620040-02_CGCTGATC - IEM All Panels
 #java -jar GenomeAnalysisTK.jar \
-	-T RealignerTargetCreator \
-	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
-	-L ../../example_fastqs/beds/IEM_all_panels_header.bed \
-	-I ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag_RG.bam \
-	-o ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag_RG.intervals
+#	-T RealignerTargetCreator \
+#	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
+#	-L ../../example_fastqs/beds/IEM_all_panels_header.bed \
+#	-I ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag_RG.bam \
+#	-o ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag_RG.intervals
 #
 #1703057-S1705957-02_AAGACGGA - HeredCancer full
 #java -jar GenomeAnalysisTK.jar \
-	-T RealignerTargetCreator \
-	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
-	-L ../../example_fastqs/beds/HeredCancer_full_panel_25bp_v1.bed \
-	-I ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag_RG.bam \
-	-o ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag_RG.intervals
+#	-T RealignerTargetCreator \
+#	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
+#	-L ../../example_fastqs/beds/HeredCancer_full_panel_25bp_v1.bed \
+#	-I ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag_RG.bam \
+#	-o ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag_RG.intervals
 
 
 ##  INDEL REALIGNMENT Test run
 #cd ../GenomeAnalysisTK-3.6
 #java -jar GenomeAnalysisTK.jar \
-	-T IndelRealigner \
-	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
-	-L ../../example_fastqs/beds/NGD_CompletePanel_25bp_v2.bed \
-	-I ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG.bam \
-	-targetIntervals ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG.intervals \
-	-o ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned.bam
+#	-T IndelRealigner \
+#	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
+#	-L ../../example_fastqs/beds/NGD_CompletePanel_25bp_v2.bed \
+#	-I ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG.bam \
+#	-targetIntervals ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG.intervals \
+#	-o ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned.bam
 
 
 ##########################################################
@@ -286,26 +286,26 @@
 #1504850-S1509352-02_GCTCGGTA - required bed files
 #cd ../picard-tools-2.5.0
 #java -jar picard.jar BedToIntervalList \
-	I=../../example_fastqs/beds/NGD_CompletePanel_25bp_v2.bed \
-	O=../../home/stpuser/bed_intervals/NGD_CompletePanel_25bp_v2.intervals \
-	SD=../../reference_files/ucsc.hg19.nohap.masked.dict
+#	I=../../example_fastqs/beds/NGD_CompletePanel_25bp_v2.bed \
+#	O=../../home/stpuser/bed_intervals/NGD_CompletePanel_25bp_v2.intervals \
+#	SD=../../reference_files/ucsc.hg19.nohap.masked.dict
 #java -jar picard.jar BedToIntervalList \
-	I=../../example_fastqs/beds/NGD_dystonia_v3_25bp.bed \
-	O=../../home/stpuser/bed_intervals/NGD_dystonia_v3_25bp.intervals \
-	SD=../../reference_files/ucsc.hg19.nohap.masked.dict
-
+#	I=../../example_fastqs/beds/NGD_dystonia_v3_25bp.bed \
+#	O=../../home/stpuser/bed_intervals/NGD_dystonia_v3_25bp.intervals \
+#	SD=../../reference_files/ucsc.hg19.nohap.masked.dict
+#
 
 #Using master bed as the bait region for now. Test run.
 ##  Picard HsMetrics tools for 30X coverage
 #1504850-S1509352-02_GCTCGGTA
 #cd ../picard-tools-2.5.0
 #java -jar picard.jar CollectHsMetrics \
-	I=../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned.bam \
-	o=../../home/stpuser/aligned_seqs/1504850-S1509352-02_hsmetrics.txt \
-	R=../../reference_files/ucsc.hg19.nohap.masked.fasta \
-	BAIT_INTERVALS=../../home/stpuser/bed_intervals/NGD_CompletePanel_25bp_v2.intervals \
-	TARGET_INTERVALS=../../home/stpuser/bed_intervals/NGD_dystonia_v3_25bp.intervals
-
+#	I=../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned.bam \
+#	o=../../home/stpuser/aligned_seqs/1504850-S1509352-02_hsmetrics.txt \
+#	R=../../reference_files/ucsc.hg19.nohap.masked.fasta \
+#	BAIT_INTERVALS=../../home/stpuser/bed_intervals/NGD_CompletePanel_25bp_v2.intervals \
+#	TARGET_INTERVALS=../../home/stpuser/bed_intervals/NGD_dystonia_v3_25bp.intervals
+#
 	
 ## Use Sambamba for exonic coverage
 ## DOESN'T WORK YET
@@ -313,22 +313,55 @@
 #1504850-S1509352-02_GCTCGGTA
 #cd ../sambamba_v0.6.3
 #sambamba depth region -L ../../example_fastqs/beds/NGD_dystonia_v3_25bp.bed -T 30 -m -F “mapping_quality >= 30” ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned.bam > ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned.sambamba_output.bed
+#
+
+##################################
+# VARIANT CALLING AND ANNOTATING #
+##################################
+#Call variants with GATK haplotype caller
+#cd ../GenomeAnalysisTK-3.6
+#java -jar GenomeAnalysisTK.jar \
+#	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
+#	-T HaplotypeCaller \
+#	-I ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned.bam \
+#	-L /example_fastqs/beds/NGD_dystonia_v3_25bp.bed \
+#	-stand_call_conf 30 \
+#	--output_mode EMIT_VARIANTS_ONLY \
+#	-maxAltAlleles 10 \
+#	-o ../../home/stpuser/vcf/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned_variants.vcf
+
+#Annotate with SnpEff (used in our pipeline)
+cd ../snpEff
+#Look at human databases - GRCh37.75 available
+#java -jar snpEff.jar databases | less | grep "Homo*" 
+#Annotate to human genome 37 with dbSNP annotations. Specify intervals broad bed. DON'T ADD dbSNP NUMBERS YET!
+java -Xmx4g -jar snpEff.jar \
+-fi ../../example_fastqs/beds/NGD_dystonia_v3_25bp.bed \
+GRCh37.75 \
+../../home/stpuser/vcf/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned_variants.vcf > ../../home/stpuser/vcf/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned_variants_ann.vcf
+
+#Next: decompose the ALT column to put second alleles on a spare row, and then annotate with dbSNP
+#Filter out those with dbSNP frequency above 5%
 
 
-###################
-# VARIANT CALLING #
-###################
-#Use GATK haplotype caller
-cd ../GenomeAnalysisTK-3.6
-java -jar GenomeAnalysisTK.jar \
-	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
-	-T HaplotypeCaller \
-	-I ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned.bam \
-	-L ../../example_fastqs/beds/NGD_CompletePanel_25bp_v2.bed \
-	-stand_call_conf 30 \
-	--output_mode EMIT_VARIANTS_ONLY \
-	-maxAltAlleles 10 \
-	-o ../../home/stpuser/vcf/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned_variants.vcf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
