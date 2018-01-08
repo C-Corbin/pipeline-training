31a6b6f3a145b6cb9f09eeadac90f5be29724ab0
Corbin <31924492+C-Corbin@users.noreply.github.com>
2018-01-08 14:27:41 +0000

    Prepare for log file generation
    Echo lines added for each item of software, so that when the script is executed with console output directed to a text file, it includes run times, dates and software versions

--- cat_lanes_and_align.sh
+++ cat_lanes_and_align.sh
@@ -1,23 +1,40 @@
+#Start in software folder
+#Print start date and time for the console record
+echo Pipeline run began at: $(date)
+echo ''
+
+
 #################
 #    FASTQC     #
 #################
 
 #Start in software folder
-#Run Fastqc and write to own folder
+#Run Fastqc and write to own folder, output version number, date and times to log file
 #cd FastQC
+#./fastqc -v 
+#echo FastQC analysis start time: $(date)
 #./fastqc ../../example_fastqs/*fastq.gz --outdir=/home/stpuser/fastqcs
+#echo FastQC analysis end time: $(date)
+#echo ''
 
 
 #########################
 # ALIGN AND MERGE LANES #
 #########################
-#Merging lanes after alignment is more 'best practice'
+#Merging lanes after alignment conforms more to 'best practice'
 
 #Run bwa mem on R1 R2 L001 L002 for the samples, write to ../../home/stpuser/aligned_seqs
 #The genome reference is in ../../reference_files/ucsc.hg19.nohap.masked.fasta
-#cd ../software/bwa-0.7.15
-#
-#
+
+#echo bwa mem and samtools, align and merge lanes
+#cd ..
+#echo samtools version:
+#samtools --version
+#cd bwa-0.7.15
+#echo bwa mem version:
+#bwa 2>&1 |grep Version | cut -f2 -d " "
+#echo bwa mem and samtools start time: $(date)
+
 #Sample 1504850-S1509352-02_GCTCGGTA
 #bwa mem ../../reference_files/ucsc.hg19.nohap.masked.fasta ../../example_fastqs/1504850-S1509352-02_GCTCGGTA_L001_R1_001.fastq.gz ../../example_fastqs/1504850-S1509352-02_GCTCGGTA_L001_R2_001.fastq.gz | samtools view -bh -o ../../home/stpuser/aligned_seqs/temp_L001.bam
 #bwa mem ../../reference_files/ucsc.hg19.nohap.masked.fasta ../../example_fastqs/1504850-S1509352-02_GCTCGGTA_L002_R1_001.fastq.gz ../../example_fastqs/1504850-S1509352-02_GCTCGGTA_L002_R2_001.fastq.gz | samtools view -bh -o ../../home/stpuser/aligned_seqs/temp_L002.bam
@@ -76,58 +93,48 @@
 ##Delete temporary files
 #rm ../../home/stpuser/aligned_seqs/temp_L001.bam
 #rm ../../home/stpuser/aligned_seqs/temp_L002.bam
-
-
-######################################
-# DEFUNCT; CONCATENATING NOT MERGING #
-######################################
-
-#DEFUNCT: Concatenate lanes, then run bwa mem - this gets rid of read group information
-#bwa mem ../../reference_files/ucsc.hg19.nohap.masked.fasta ../../home/stpuser/joined_lanes/1504850-S1509352-02_GCTCGGTA_R1_both.fastq.gz ../../home/stpuser/joined_lanes/1504850-S1509352-02_GCTCGGTA_R2_both.fastq.gz | samtools view -bh -o ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA.bam
-#bwa mem ../../reference_files/ucsc.hg19.nohap.masked.fasta ../../home/stpuser/joined_lanes/1606034-S1612259-02_CGAACTTA_R1_both.fastq.gz ../../home/stpuser/joined_lanes/1606034-S1612259-02_CGAACTTA_R2_both.fastq.gz | samtools view -bh -o ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA.bam
-#bwa mem ../../reference_files/ucsc.hg19.nohap.masked.fasta ../../home/stpuser/joined_lanes/1607686-S1615531-02_TTCACGCA_R1_both.fastq.gz ../../home/stpuser/joined_lanes/1607686-S1615531-02_TTCACGCA_R2_both.fastq.gz | samtools view -bh -o ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA.bam
-#bwa mem ../../reference_files/ucsc.hg19.nohap.masked.fasta ../../home/stpuser/joined_lanes/1609778-S1620040-02_CGCTGATC_R1_both.fastq.gz ../../home/stpuser/joined_lanes/1609778-S1620040-02_CGCTGATC_R2_both.fastq.gz | samtools view -bh -o ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC.bam
-#bwa mem ../../reference_files/ucsc.hg19.nohap.masked.fasta ../../home/stpuser/joined_lanes/1703057-S1705957-02_AAGACGGA_R1_both.fastq.gz ../../home/stpuser/joined_lanes/1703057-S1705957-02_AAGACGGA_R1_both.fastq.gz | samtools view -bh -o ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA.bam
-
-
-#DEFUNCT: Join lanes 1 and 2, writes to the joined_lanes folder 
-#cat 1504850-S1509352-02_GCTCGGTA_L001_R1_001.fastq.gz 1504850-S1509352-02_GCTCGGTA_L002_R1_001.fastq.gz > ../home/stpuser/joined_lanes/1504850-S1509352-02_GCTCGGTA_R1_both.fastq.gz
-#cat 1504850-S1509352-02_GCTCGGTA_L001_R2_001.fastq.gz 1504850-S1509352-02_GCTCGGTA_L002_R2_001.fastq.gz > ../home/stpuser/joined_lanes/1504850-S1509352-02_GCTCGGTA_R2_both.fastq.gz
-#cat 1606034-S1612259-02_CGAACTTA_L001_R1_001.fastq.gz 1606034-S1612259-02_CGAACTTA_L002_R1_001.fastq.gz > ../home/stpuser/joined_lanes/1606034-S1612259-02_CGAACTTA_R1_both.fastq.gz
-#cat 1606034-S1612259-02_CGAACTTA_L001_R2_001.fastq.gz 1606034-S1612259-02_CGAACTTA_L002_R2_001.fastq.gz > ../home/stpuser/joined_lanes/1606034-S1612259-02_CGAACTTA_R2_both.fastq.gz
-#cat 1607686-S1615531-02_TTCACGCA_L001_R1_001.fastq.gz 1607686-S1615531-02_TTCACGCA_L002_R1_001.fastq.gz > ../home/stpuser/joined_lanes/1607686-S1615531-02_TTCACGCA_R1_both.fastq.gz
-#cat 1607686-S1615531-02_TTCACGCA_L001_R2_001.fastq.gz 1607686-S1615531-02_TTCACGCA_L002_R2_001.fastq.gz > ../home/stpuser/joined_lanes/1607686-S1615531-02_TTCACGCA_R2_both.fastq.gz
-#cat 1609778-S1620040-02_CGCTGATC_L001_R1_001.fastq.gz 1609778-S1620040-02_CGCTGATC_L002_R1_001.fastq.gz > ../home/stpuser/joined_lanes/1609778-S1620040-02_CGCTGATC_R1_both.fastq.gz
-#cat 1609778-S1620040-02_CGCTGATC_L001_R2_001.fastq.gz 1609778-S1620040-02_CGCTGATC_L002_R2_001.fastq.gz > ../home/stpuser/joined_lanes/1609778-S1620040-02_CGCTGATC_R2_both.fastq.gz
-#cat 1703057-S1705957-02_AAGACGGA_L001_R1_001.fastq.gz 1703057-S1705957-02_AAGACGGA_L002_R1_001.fastq.gz > ../home/stpuser/joined_lanes/1703057-S1705957-02_AAGACGGA_R1_both.fastq.gz
-#cat 1703057-S1705957-02_AAGACGGA_L001_R2_001.fastq.gz 1703057-S1705957-02_AAGACGGA_L002_R2_001.fastq.gz > ../home/stpuser/joined_lanes/1703057-S1705957-02_AAGACGGA_R2_both.fastq.gz
+#echo bwa mem and samtools end time: $(date)
+#echo ''
 
 
 #########################
 #  SORT AND INDEX SEQS  #
 #########################
 
+#cd ..
+#echo Sort and index files
+#echo samtools version:
+#samtools --version
+#cd samtools-1.3.1
+#echo samtools sort start time: $(date)
 #Sort the files
-#cd ../samtools-1.3.1
 #samtools sort -o ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted.bam ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA.bam
 #samtools sort -o ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted.bam ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA.bam
 #samtools sort -o ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted.bam ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA.bam
 #samtools sort -o ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted.bam ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA.bam
 #samtools sort -o ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted.bam ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC.bam
 #samtools sort -o ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted.bam ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA.bam
+#echo samtools sort end time: $(date)
+#echo ''
 
+#echo samtools index start time: $(date)
 #Index the files for IGV viewing
 #samtools index ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted.bam
 #samtools index ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted.bam
 #samtools index ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted.bam
 #samtools index ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted.bam
 #samtools index ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted.bam
+#echo samtools index end time: $(date)
+#echo ''
 
 
 ###################
 # MARK DUPLICATES #
 ###################
 
+#echo Mark duplicates
+#echo Picard version 2.5.0
+#echo Picard MarkDuplicates start time: $(date)
 #I need to mark duplicates for each file
 #This also writes a metrics file for each flagged file
 #cd ../picard-tools-2.5.0 
@@ -136,12 +143,18 @@
 #java -jar picard.jar MarkDuplicates I=../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted.bam O=../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag.bam M=../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag_metrics.txt
 #java -jar picard.jar MarkDuplicates I=../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted.bam O=../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag.bam M=../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag_metrics.txt
 #java -jar picard.jar MarkDuplicates I=../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted.bam O=../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag.bam M=../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag_metrics.txt
+#echo Picard MarkDuplicates end time: $(date)
+#echo ''
 
 
 ############
 # FLAGSTAT #
 ############
 
+#echo Run flagstat quality metrics
+#echo samtools version:
+#samtools --version
+#echo samtools flagstat start time: $(date)
 #samtools flagstat for quality metrics - to see how they match up with the picard ones!
 #cd ../samtools-1.3.1
 #samtools flagstat ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag.bam > ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_flagstat.txt
@@ -149,6 +162,8 @@
 #samtools flagstat ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag.bam > ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag_flagstat.txt
 #samtools flagstat ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag.bam > ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag_flagstat.txt
 #samtools flagstat ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag.bam > ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag_flagstat.txt
+#echo samtools flagstat end time: $(date)
+#echo ''
 
 
 ###################
@@ -158,6 +173,9 @@
 #However in our 4-sample example we can probably put in placeholder read groups
 #Adding them quite late - 'real' ones would be added early, while still at the lanes-and-reads stage
 
+#echo Add read groups
+#echo Picard version 2.5.0
+#echo Picard AddOrReplaceReadGroups start time: $(date)
 #1504850-S1509352-02_GCTCGGTA
 #cd ../picard-tools-2.5.0
 #java -jar picard.jar AddOrReplaceReadGroups \
@@ -209,25 +227,41 @@
 	RGPU=unit1 \
 	RGSM=1703057-S1705957-02_AAGACGGA
 	
+#echo Picard AddOrReplaceReadGroups end time: $(date)
+#echo ''
+
 
 #####################
 # INDEL REALIGNMENT #
 #####################
 
-###INDEXING - needed for indel-realignment files
 #cd ../samtools-1.3.1
+#echo Indel realignment: index files with samtools index
+#echo samtools version:
+#samtools --version
+#echo samtools index start time: $(date)
+
+###INDEXING - needed for indel-realignment files
 #samtools index ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG.bam
 #samtools index ../../home/stpuser/aligned_seqs/1606034-S1612259-02_CGAACTTA_sorted_dupflag_RG.bam
 #samtools index ../../home/stpuser/aligned_seqs/1607686-S1615531-02_TTCACGCA_sorted_dupflag_RG.bam
 #samtools index ../../home/stpuser/aligned_seqs/1609778-S1620040-02_CGCTGATC_sorted_dupflag_RG.bam
 #samtools index ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag_RG.bam
 
+#echo samtools index end time: $(date)
+#echo ''
+
+#cd ../GenomeAnalysisTK-3.6
+#echo Indel realignment: identify target regions with GATK RealignerTargetCreator
+#echo GenomeAnalysisTK version:
+#java -jar GenomeAnalysisTK.jar -version
+#echo GenomeAnalysisTK RealignerTargetCreator start time: $(date)
 
 ### IDENTIFY TARGET REGIONS
 ### CAN RESTRICT TO THE BROAD BED FILE WITH -L, the GATK 'intervals' option
 #
 #1504850-S1509352-02_GCTCGGTA - NGD Complete
-#cd ../GenomeAnalysisTK-3.6
+
 #java -jar GenomeAnalysisTK.jar \
 #	-T RealignerTargetCreator \
 #	-R ../../reference_files/ucsc.hg19.nohap.masked.fasta \
@@ -266,6 +300,14 @@
 #	-I ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag_RG.bam \
 #	-o ../../home/stpuser/aligned_seqs/1703057-S1705957-02_AAGACGGA_sorted_dupflag_RG.intervals
 
+#echo GenomeAnalysisTK RealignerTargetCreator end time: $(date)
+#echo ''
+
+#cd ../GenomeAnalysisTK-3.6
+#echo Indel realignment: perform the realignment with GATK IndelRealigner
+#echo GenomeAnalysisTK version:
+#java -jar GenomeAnalysisTK.jar -version
+#echo GenomeAnalysisTK IndelRealigner start time: $(date)
 
 ##  INDEL REALIGNMENT Test run
 #cd ../GenomeAnalysisTK-3.6
@@ -276,6 +318,9 @@
 #	-I ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG.bam \
 #	-targetIntervals ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG.intervals \
 #	-o ../../home/stpuser/aligned_seqs/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned.bam
+
+#echo GenomeAnalysisTK IndelRealigner end time: $(date)
+#echo ''
 
 
 ##########################################################
@@ -345,22 +390,21 @@
 #../../home/stpuser/vcf/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned_variants_decomp.vcf > \
 #../../home/stpuser/vcf/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned_variants_decomp_ann.vcf
 #
-#SnpSift for dbSNP
-cd ../snpEff
-java -jar SnpSift.jar annotate ../../reference_files/dbsnp/common_all_20161122.vcf.gz \
- ../../home/stpuser/vcf/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned_variants_decomp.vcf > \
-../../home/stpuser/vcf/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned_variants_decomp_dbsnp.vcf
-#Ran, but don't think rs number added correctly
+#SnpSift for ExAc
+#cd ../snpEff
+#java -jar SnpSift.jar annotate -v ../../reference_files/exac/ExAC.r0.3.1.sites.vep.vcf.gz \
+# ../../home/stpuser/vcf/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned_variants_decomp.vcf > \
+#../../home/stpuser/vcf/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned_variants_decomp_exac.vcf
 
-#Next: fix the dbSNP annotations
 #After: filter variants above 5%
+#cd ../snpEff
+#cat ../../home/stpuser/vcf/1504850-S1509352-02_GCTCGGTA_sorted_dupflag_RG_realigned_variants_decomp_exac.vcf | java -jar #SnpSift.jar filter "(#filtered thing)" | > \ 
+#../../home/stpuser/vcf/filtered.vcf
 
 
-
-
-
-
-
+#Print end date and time for the console record
+echo '' 
+echo Pipeline run completed at: $(date)
 
 
 
